a4d7358be66a0343e12ca28c682e460dcc34e7fe
Ikerm01 <151910000+Ikerm01@users.noreply.github.com>
2024-03-04 13:02:45 +0100

    hecho topdown

--- agencia_viajes.kt
+++ agencia_viajes.kt
@@ -1,1 +1,35 @@
-...
+fun main():
+  afegir_llista_viatge()
+  pedir_informacio()
+  mostrar_informacio_viatge()
+  regristra_client()
+  fer_reserva()
+  mostrar_reserva()
+  mostrar_opcions()
+  tractar_opcions()
+
+
+  
+fun afegir_llista_viatge():
+  ...
+
+fun pedir_informacio():
+  ...
+
+fun mostrar_informacio_viatge():
+  ...
+
+fun regristra_client():
+  ...
+
+fun fer_reserva():
+  ...
+
+fun mostrar_reserva():
+  ...
+
+fun mostrar_opcions():
+  ...
+
+fun tractar_opcions():
+  ...
